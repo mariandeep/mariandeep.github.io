b76f279973fba1ed1ba36054a8f896121bb29143
Alexey Belkin <alexey.belkin@mail.ru>
2024-09-01 01:10:29 +0800

    connect service worker in code

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,12 @@
+import { onMount } from "solid-js";
 import { render } from "solid-js/web";
 
 function App() {
+  onMount(()=> {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js')
+    };
+  });
   return (
     <h2>
         Hello Oleg
