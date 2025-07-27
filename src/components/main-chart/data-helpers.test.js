a7c97e62f72c06d5c1e66107b2d6215a3f0e4cdd
Alexey Belkin <alexey.belkin@mail.ru>
2025-07-27 18:49:34 +0800

    update tests to specific date

--- src/components/main-chart/data-helpers.test.js
+++ src/components/main-chart/data-helpers.test.js
@@ -5,29 +5,31 @@ import secrets from './../../secrets.json';
 import env from './../../env.json';
 
 describe('Data adapter tests', async () => {
+    const date = new Date(2025, 4, 13, 0, 0, 0, 0);
     it('should create proper body', () => {
-        const body = getPageableBody(new Date(2025, 7, 13, 0, 0, 0, 0), 1, 1, 10);
+        const body = getPageableBody(date, 1, 1, 10);
         expect(body).toEqual({
-            startDate: '2025-08-12T16:00:00.000Z',
-            endDate: '2025-08-11T16:00:00.000Z',
+            startDate: '2025-05-12T16:00:00.000Z',
+            endDate: '2025-05-11T16:00:00.000Z',
             pageNumber: 1,
             pageSize: 10,
         });
     });
     it('should return glucose readings for today', async () => {
-        const dataOrError = await postData(env.Endpoints.Glucose, secrets.User, getPageableBody(new Date(), 7));
+        const dataOrError = await postData(env.Endpoints.Glucose, secrets.User, getPageableBody(date, 7));
         expect(dataOrError).toBeDefined();
         expect(dataOrError).toBeInstanceOf(Object);
     });
     it('should return insuline readings for today', async () => {
-        const dataOrError = await postData(env.Endpoints.Insulin, secrets.User, getPageableBody(new Date(), 7));
+        const dataOrError = await postData(env.Endpoints.Insulin, secrets.User, getPageableBody(date, 7));
         expect(dataOrError).toBeDefined();
         expect(dataOrError).toBeInstanceOf(Object);
+        console.log(dataOrError);
     });
     it('should return error', async () => {
         let result;
         try {
-            result = await postData(env.Endpoints.Insulin, secrets.User, getPageableBody(new Date(), 7));
+            result = await postData(env.Endpoints.Insulin, secrets.User, getPageableBody(date, 7));
         } finally {
             expect(result).toBeDefined();
         }
